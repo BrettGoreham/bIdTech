0dd672566313977d4fddf8548bda3fd3b29974ef
Brett Goreham <brettgoreham@gmail.com>
2025-02-05 20:23:27 +0100

    adding base packages after refactoring move

--- src/main/java/bankid/techinterview/application/TechinterviewApplication.java
+++ src/main/java/bankid/techinterview/application/TechinterviewApplication.java
@@ -2,8 +2,9 @@ package bankid.techinterview.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"bankid.techinterview"})
 public class TechinterviewApplication {
 
 	public static void main(String[] args) {
